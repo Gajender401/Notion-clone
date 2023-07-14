c508dcac58563971f9959fc93e29109bff40e97f
Gajender <kgajender86@gmail.com>
2023-07-14 18:30:58 +0530

    first commit

---
+++ app/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+      <header>
+        <h2 className="p-[1rem] text-slate-100 text-3xl font-bold bg-slate-700">
+          Notion
+        </h2>
+      </header>
+    );
+  }
---
+++ app/pages/topic/page.tsx
@@ -0,0 +1,7 @@
+export default function Topic() {
+    return (
+      <main className="flex flex-col">
+        <h2>Topic</h2>
+      </main>
+    );
+  }
